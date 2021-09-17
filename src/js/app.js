c2557f8df08341d8a3699426a69e17aaa92a6ac0
Daniel <daniel_sh.99@abv.bg>
2021-09-18 01:37:17 +0300

    Pokemon names display app

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,16 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+    const ul = document.querySelector("ul");
+
+  for (let i = 1; i <= 10; i++) {
+      // eslint-disable-next-line no-undef
+    fetch('https://pokeapi.co/api/v2/pokemon/'+i)
+        .then(response => response.json())
+        .then(json => {
+            let li = document.createElement('li');
+            li.appendChild(document.createTextNode(json.name));
+            ul.appendChild(li);
+        });
+  }
 });
